05bf771fbb36d218de91055a4d21bb54638a50d9
lulu <s987337613@gmail.com>
2017-04-07 23:58:29 +0800

    add size and color dictionary

--- RefactoringKata/OrdersWriter.cs
+++ RefactoringKata/OrdersWriter.cs
@@ -1,10 +1,29 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace RefactoringKata
 {
     public class OrdersWriter
     {
         private Orders _orders;
+        private Dictionary<int, string> _size = new Dictionary<int, string>()
+        {
+            {-1,"Invalid Size"},
+            {1,"XS"},
+            {2,"S"},
+            {3,"M"},
+            {4,"L"},
+            {5,"XL"},
+            {6,"XXL"},
+        };
+        private Dictionary<int, string> _color = new Dictionary<int, string>()
+        {
+            {-1,"no color"},
+            { 1,"blue"},
+            { 2,"red"},
+            { 3,"yellow"}
+        };
 
         public OrdersWriter(Orders orders)
         {
@@ -13,94 +32,56 @@ namespace RefactoringKata
 
         public string GetContents()
         {
+
             var sb = new StringBuilder("{\"orders\": [");
 
             for (var i = 0; i < _orders.GetOrdersCount(); i++)
             {
                 var order = _orders.GetOrder(i);
-                sb.Append("{");
-                sb.Append("\"id\": ");
+                sb.Append("{\"id\": ");
                 sb.Append(order.GetOrderId());
-                sb.Append(", ");
-                sb.Append("\"products\": [");
+                sb.Append(", \"products\": [");
 
                 for (var j = 0; j < order.GetProductsCount(); j++)
                 {
-                    var product = order.GetProduct(j);
-                    sb.Append("{");
-                    sb.Append("\"code\": \"");
-                    sb.Append(product.Code);
-                    sb.Append("\", ");
-                    sb.Append("\"color\": \"");
-                    sb.Append(getColorFor(product));
-                    sb.Append("\", ");
-
-                    if (product.Size != Product.SIZE_NOT_APPLICABLE)
-                    {
-                        sb.Append("\"size\": \"");
-                        sb.Append(getSizeFor(product));
-                        sb.Append("\", ");
-                    }
-
-                    sb.Append("\"price\": ");
-                    sb.Append(product.Price);
-                    sb.Append(", ");
-                    sb.Append("\"currency\": \"");
-                    sb.Append(product.Currency);
-                    sb.Append("\"}, ");
+                    sb.Append(GenerateProduct(order.GetProduct(j)));
+                    RemoveLastCharacter(sb, _orders.GetOrdersCount());
+                    sb.Append("}, ");
                 }
+                RemoveLastCharacter(sb, order.GetProductsCount());
 
-                if (order.GetProductsCount() > 0)
-                {
-                    sb.Remove(sb.Length - 2, 2);
-                }
-
-                sb.Append("]");
-                sb.Append("}, ");
+                sb.Append("]}, ");
             }
 
-            if (_orders.GetOrdersCount() > 0)
-            {
-                sb.Remove(sb.Length - 2, 2);
-            }
+            RemoveLastCharacter(sb, _orders.GetOrdersCount());
 
             return sb.Append("]}").ToString();
         }
 
-
-        private string getSizeFor(Product product)
+        private string GenerateProduct(Product product)
         {
-            switch (product.Size)
+            var productMappin = new Dictionary<string, string>()
             {
-                case 1:
-                    return "XS";
-                case 2:
-                    return "S";
-                case 3:
-                    return "M";
-                case 4:
-                    return "L";
-                case 5:
-                    return "XL";
-                case 6:
-                    return "XXL";
-                default:
-                    return "Invalid Size";
+                {"code","\""+product.Code+"\""},
+                {"color","\""+_color[product.Color]+"\""},
+                {"size", "\"" + _size[product.Size]+"\""},
+                {"price",product.Price.ToString()},
+                {"currency","\""+product.Currency+"\""}
+            };
+            if (product.Size == Product.SIZE_NOT_APPLICABLE)
+            {
+                productMappin.Remove("size");
             }
+
+            return productMappin.Aggregate("{", (current, item) => current + string.Format("\"{0}\": {1}, ", item.Key, item.Value));
+
         }
 
-        private string getColorFor(Product product)
+        private void RemoveLastCharacter(StringBuilder sb, int ordersCount)
         {
-            switch (product.Color)
+            if (ordersCount > 0)
             {
-                case 1:
-                    return "blue";
-                case 2:
-                    return "red";
-                case 3:
-                    return "yellow";
-                default:
-                    return "no color";
+                sb.Remove(sb.Length - 2, 2);
             }
         }
     }
